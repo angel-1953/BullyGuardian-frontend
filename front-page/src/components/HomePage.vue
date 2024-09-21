024e0b1235bdea9d7d044b1a6c93c73a4a8b4127
FangYu <ericayu911025@gmail.com>
2024-09-21 19:40:31 +0800

    header

--- front-page/src/components/HomePage.vue
+++ front-page/src/components/HomePage.vue
@@ -1,38 +1,55 @@
 <template>
-  <!-- 整個 header 容器 -->
+
   <header class="header">
-    <!-- 左側部分：logo 和名稱 -->
+
     <div class="logo">
-      <!-- Logo 圖片來源 -->
-      <img src="@/assets/logo.png" alt="Campus Bully Guardian Logo" />
-      <h1>校園凌制零</h1>
-      <span>Campus Bully Guardian</span>
+      <img src="./assets/Final.png" alt="Campus Bully Guardian Logo" style="width: 70%;"/>
     </div>
-    
-    <!-- 中間部分：導航選單 -->
-    <nav class="nav">
+
+    <div class="login_register">
+      <a href="#" class="login">登入</a>
+      <a href="#" class="register">註冊</a>
+    </div>
+
+  </header>
+  
+  <nav class="nav">
       <ul>
         <li><a href="#">事件通報門</a></li>
         <li><a href="#">霸凌雷達站</a></li>
-        <!-- 包含下拉式選單 -->
+
         <li class="dropdown">
-          <a href="#">素養成長坊 <span class="arrow">▼</span></a>
+          <a href="#"><span class="arrow">素養成長坊 ▼</span></a>
           <ul class="dropdown-menu">
             <li><a href="#">影片欣賞</a></li>
             <li><a href="#">實力測驗</a></li>
           </ul>
         </li>
-        <li><a href="#">資訊導航室</a></li>
-      </ul>
-    </nav>
 
-    <!-- 右側部分：登入和註冊 -->
-    <div class="auth">
-      <a href="#" class="login">登入</a>
-      <a href="#" class="register">註冊</a>
-    </div>
-  </header>
+        <li><a href="#">資訊導航室</a></li>
+
+      </ul>
+  </nav>
+
+<body style="background-color:aqua;">
+
+  <h1>會員登入</h1>
+
+  <div class="title_login">
+    <h2>會員登入</h2>
+  </div>
+  <div class="title_register">
+    <h2>會員註冊</h2>
+  </div>
+
+  <div class="content">
+
+  </div>
+
+</body>
+
 </template>
 
 <script src="./HomePage.js"></script>
-<style src="./HomePage.css" scoped></style>
+<style src="./header.css" ></style>
+<style src="./login.css" ></style>
