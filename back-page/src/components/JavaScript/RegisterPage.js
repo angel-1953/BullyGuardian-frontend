c07c380e5febb5a28777fa994a480803983386a6
s1411031013 <grong9913@gmail.com>
2024-10-08 22:43:49 +0800

    202410080003

--- back-page/src/components/JavaScript/RegisterPage.js
+++ back-page/src/components/JavaScript/RegisterPage.js
@@ -2,6 +2,34 @@ export default {
     name: "RegisterPage",
     mounted() {
       document.title = "校園凌制零-註冊";
+    },
+    data() {
+      return {
+        form: {
+          account: '',
+          password: '',
+          confirmPassword: '',
+          email: '',
+          name: '',
+          school: '',
+          photo: null
+        }
+      };
+    },
+    methods: {
+      submitForm() {
+        // Check if passwords match
+        if (this.form.password !== this.form.confirmPassword) {
+          alert("Passwords do not match!");
+          return;
+        }
+        // Submit form logic
+        console.log("Form submitted", this.form);
+      },
+      handleFileUpload(event) {
+        this.form.photo = event.target.files[0];
+        console.log("File uploaded", this.form.photo);
+      }
     }
   };
 
--- back-page/src/components/RegisterPage.vue
+++ back-page/src/components/RegisterPage.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="app-container">
-    <header class="header">
-      <div class="logo">
-            <img src="./assets/Final.png" alt="Campus Bully Guardian Logo" style="width: 30%;"/>
+    <header>
+      <img src="./assets/Final.png" alt="Logo" class="logo" width="20%"/>
+      <div class="header-button">
+        
+          <router-link to="/events" class="link-button">
+            <button class="button">
+              登入
+            </button>
+          </router-link>
+        
+          <router-link to="/events" class="link-button">
+            <button class="button">
+              註冊
+            </button>
+          </router-link>
       </div>
     </header>
     <div class="main-container">
@@ -10,51 +22,97 @@
         <nav>
           <ul>
             <li>
-              <a href="#">
-                <img src="./assets/個人頭像.png" alt="Personal" class="icon" />
-                XXX，您好
-              </a>
+              <router-link to="/events" class="link">
+                <img src="./assets/個人頭像.png" class="icon" />
+                XXX,您好
+              </router-link>
             </li>
             <li>
-              <a href="#">
-                <img src="./assets/事件檢視.png" alt="Personal" class="icon" />
+              <router-link to="/events" class="link">
+                <img src="./assets/事件檢視.png" class="icon" />
                 事件檢視
-              </a>
+              </router-link>
             </li>
             <li>
-              <a href="#">
-                <img src="./assets/使用者資訊.png" alt="Personal" class="icon" />
+              <router-link to="/user-info" class="link">
+                <img src="./assets/使用者資訊.png" class="icon" />
                 使用者資訊
-              </a>
+              </router-link>
             </li>
             <li>
-              <a href="#">
-                <img src="./assets/數據統計.png" alt="Personal" class="icon" />
+              <router-link to="/stats" class="link">
+                <img src="./assets/數據統計.png" class="icon" />
                 數據統計
-              </a>
+              </router-link>
             </li>
             <li>
-              <a href="#">
-                <img src="./assets/網路資訊.png" alt="Personal" class="icon" />
+              <router-link to="/network-info" class="link">
+                <img src="./assets/網路資訊.png" class="icon" />
                 網路資訊
-              </a>
+              </router-link>
             </li>
             <li>
-              <a href="#">
-                <img src="./assets/素養資料.png" alt="Personal" class="icon" />
+              <router-link to="/resources" class="link">
+                <img src="./assets/素養資料.png" class="icon" />
                 素養資料
-              </a>
+              </router-link>
             </li>
           </ul>
         </nav>
+        <footer>
+          <router-link to="/login" class="link">
+            <img src="./assets/登入.png" class="icon" />
+            登入
+          </router-link>
+        </footer>
       </aside>
       <main class="content">
-        <router-view />
+        <h2>會員註冊</h2>
+        <div class="line"> </div>
+        <div class="register-container">
+        <form @submit.prevent="submitForm">
+          <div class="form-group">
+            <label for="account">帳號：</label>
+            <input type="text" v-model="form.account" id="account" required />
+          </div>
+
+          <div class="form-group">
+            <label for="password">密碼：</label>
+            <input type="password" v-model="form.password" id="password" required />
+          </div>
+
+          <div class="form-group">
+            <label for="confirmPassword">密碼確認：</label>
+            <input type="password" v-model="form.confirmPassword" id="confirmPassword" required />
+          </div>
+
+          <div class="form-group">
+            <label for="email">E-MAIL：</label>
+            <input type="email" v-model="form.email" id="email" required />
+          </div>
+
+          <div class="form-group">
+            <label for="name">姓名：</label>
+            <input type="text" v-model="form.name" id="name" required />
+          </div>
+
+          <div class="form-group">
+            <label for="school">學校：</label>
+            <input type="text" v-model="form.school" id="school" required />
+          </div>
+
+          <div class="form-group">
+            <label for="photo">教師證明：</label>
+            <input type="file" @change="handleFileUpload" id="photo" />
+          </div>
+
+          <button type="submit">註冊</button>
+        </form>
+        </div>
       </main>
     </div>
   </div>
 </template>
   
-  <script src="./JavaScript/RegisterPage.js"></script>
+  <script scoped src="./JavaScript/RegisterPage.js"></script>
   <style src="./style/common.css" ></style>
-  <style src="./style/login.css" ></style>
