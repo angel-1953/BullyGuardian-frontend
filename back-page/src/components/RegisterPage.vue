794c6f7c64d5e23002d9bf5d1b9e0287a6b02fef
s1411031013 <grong9913@gmail.com>
2024-10-08 20:24:22 +0800

    2024100802

--- back-page/src/components/RegisterPage.vue
+++ back-page/src/components/RegisterPage.vue
@@ -11,20 +11,38 @@
           <ul>
             <li>
               <a href="#">
-                <img src="" alt="Dashboard" class="icon" />
+                <img src="./assets/個人頭像.png" alt="Personal" class="icon" />
                 XXX，您好
               </a>
             </li>
             <li>
               <a href="#">
-                <img src="items-icon.png" alt="Item Management" class="icon" />
-                項目管理
+                <img src="./assets/事件檢視.png" alt="Personal" class="icon" />
+                事件檢視
               </a>
             </li>
             <li>
               <a href="#">
-                <img src="user-icon.png" alt="User Management" class="icon" />
-                使用者管理
+                <img src="./assets/使用者資訊.png" alt="Personal" class="icon" />
+                使用者資訊
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="./assets/數據統計.png" alt="Personal" class="icon" />
+                數據統計
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="./assets/網路資訊.png" alt="Personal" class="icon" />
+                網路資訊
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img src="./assets/素養資料.png" alt="Personal" class="icon" />
+                素養資料
               </a>
             </li>
           </ul>
