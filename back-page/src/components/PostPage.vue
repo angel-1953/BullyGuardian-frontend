d597adb5f6589333f84850740503422f60425a3d
s1411031013 <grong9913@gmail.com>
2024-10-27 22:59:58 +0800

    20241027

--- back-page/src/components/PostPage.vue
+++ back-page/src/components/PostPage.vue
@@ -19,52 +19,52 @@
       </header>
       <div class="main-container">
         <aside class="sidebar">
-            <nav>
-              <ul>
-                <li>
-                  <div class="nolink">
-                    <img src="./assets/個人頭像.png" class="icon" />
-                    XXX,您好
-                  </div>
-                </li>
-                <li>
-                  <router-link to="/eventList" class="link">
-                    <img src="./assets/事件檢視.png" class="icon" />
-                    事件檢視
-                  </router-link>
-                </li>
-                <li>
-                  <router-link to="/userInfo" class="link">
-                    <img src="./assets/使用者資訊.png" class="icon" />
-                    使用者資訊
-                  </router-link>
-                </li>
-                <li>
-                  <router-link to="/managementPage" class="link">
-                    <img src="./assets/社群管理.png" class="icon" />
-                    社群管理
-                  </router-link>
-                </li>
-                <li>
-                  <router-link to="/statisticsPage" class="link">
-                    <img src="./assets/數據統計.png" class="icon" />
-                    數據統計
-                  </router-link>
-                </li>
-                <li>
-                  <router-link to="/networkInfo" class="link">
-                    <img src="./assets/網路資訊.png" class="icon" />
-                    網路資訊
-                  </router-link>
-                </li>
-                <li>
-                  <router-link to="/resourcePage" class="link">
-                    <img src="./assets/素養資料.png" class="icon" />
-                    素養資料
-                  </router-link>
-                </li>
-              </ul>
-            </nav>
+          <nav>
+            <ul>
+              <li>
+                <div class="nolink">
+                  <img src="./assets/個人頭像.png" class="icon" />
+                  {{ username }}您好
+                </div>
+              </li>
+              <li style="background-color: #f5f2ca;">
+                <router-link to="/eventList" class="link" style="background-color: #f5f2ca;">
+                  <img src="./assets/事件檢視.png" class="icon" />
+                  事件檢視
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/userInfo" class="link">
+                  <img src="./assets/使用者資訊.png" class="icon" />
+                  使用者資訊
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/managementPage" class="link">
+                  <img src="./assets/社群管理.png" class="icon" />
+                  社群管理
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/statisticsPage" class="link">
+                  <img src="./assets/數據統計.png" class="icon" />
+                  數據統計
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/networkInfo" class="link">
+                  <img src="./assets/網路資訊.png" class="icon" />
+                  網路資訊
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/resourcePage" class="link">
+                  <img src="./assets/素養資料.png" class="icon" />
+                  素養資料
+                </router-link>
+              </li>
+            </ul>
+          </nav>
             <footer>
               <router-link to="/login" class="link">
                 <img src="./assets/登出.png" class="icon" />
